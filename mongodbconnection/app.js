215009b88a9470481fa784381e8e321353b95c2c
Gaurav Maihuria <maihuriagaurav16@gmail.com>
2022-01-25 23:00:54 +0530

    populate

--- mongodbconnection/app.js
+++ mongodbconnection/app.js
@@ -28,6 +28,61 @@ const userSchema = new mongoose.Schema(
 
 const User = mongoose.model('user', userSchema)
 
+// Create the postSchema
+const tagSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+})
+
+const Tag = mongoose.model('tag', tagSchema)
+
+const postSchema = new mongoose.Schema(
+  {
+    tiltle: { type: String, required: true },
+    body: { type: String, required: true },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    tags_id: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'tag',
+        required: true,
+      },
+    ],
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  },
+)
+
+const Post = mongoose.model('post', postSchema)
+// tags schema
+
+const commentSchema = new mongoose.Schema(
+  {
+    body: { type: String, required: true },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    post_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'post',
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  },
+)
+
+const Comment = mongoose.model('comment', commentSchema)
+
 app.post('/users', async (req, res) => {
   try {
     const user = await User.create(req.body)
@@ -68,6 +123,53 @@ app.delete('/users/:id', async (req, res) => {
   }
 })
 
+// ...........................tags Crud ...............................................
+
+app.post('/tags', async (req, res) => {
+  try {
+    const tags = await Tag.create(req.body)
+    return res.status(200).send(tags)
+  } catch (e) {
+    res.status(500).json({ message: e.message, status: 'Failed' })
+  }
+})
+
+app.get('/tags', async (req, res) => {
+  try {
+    const tags = await Tag.find().lean().exec()
+    return res.send(tags)
+  } catch (e) {
+    return res.status(500).json({ message: e.message, status: 'Failed' })
+  }
+})
+
+app.get('/tags/:id', async (req, res) => {
+  try {
+    const tag = await Tag.findById(req.params.id)
+    return res.status(201).send(tag)
+  } catch (e) {
+    return res.status(500).json({ message: e, message, status: 'Failed' })
+  }
+})
+app.patch('/tags/:id', async (req, res) => {
+  try {
+    const tag = await Tag.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    })
+    return res.status(201).send(tag)
+  } catch (e) {
+    return res.status(500).json({ message: e.message, status: 'Failed' })
+  }
+})
+
+app.delete('/tags/:id', async (req, res) => {
+  try {
+    const tag = await Tag.findByIdAndDelete(req.params.id).lean().exec()
+    return res.status(200).send(tag)
+  } catch (e) {
+    return res.status(500).json({ message: e.message, status: 'Failed' })
+  }
+})
 app.listen(2345, async () => {
   await connect()
   console.log('listening to the port number 2345')
